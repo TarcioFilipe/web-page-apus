4a8a2a463193973a4bd17d994239eb537849f40c
Filipe Tarcio Herculano de Souza <filipe.herculano@akrkpromotora.local>
2025-07-11 11:11:15 -0300

    fix/ajustes em seguro de vida

--- src/components/SectionsProducts/LifeInsurance.tsx
+++ src/components/SectionsProducts/LifeInsurance.tsx
@@ -10,24 +10,29 @@ export default function LifeInsurance() {
   return (
     <section className="relative h-screen w-full flex flex-col bg-white items-center text-black overflow-hidden lg:h-[512px] xl:h-[640px] 2xl:h-[786px]">
       <div className="flex flex-col container w-full h-full lg:flex-row-reverse">
+        <div className='arrowBottomRight absolute w-[400px] h-[400px] lg:w-[800px] lg:h-[800px]' >
+          <Image src="/images/arraow_right.png" alt='Arrow right' fill objectFit='cover'/>
+        </div>
+
+
         <div className="flex flex-col w-full h-[40%] lg:h-full lg:justify-center lg:w-6/12">
           <motion.div
             initial={{ opacity: 0, y: 60}}
             whileInView={{ opacity: 1, y: 0}}
             transition={{ duration: 0.6 }}
-            className="p-4 text-center lg:text-left"
+            className="flex flex-col p-4 text-center items-center lg:text-left"
           >
             <h1 className="text-3xl lg:text-4xl/12 2xl:text-5xl/14 font-black mb-4">
               Seguro de vida
             </h1>
 
-            <p className="w-full font-light text-md lg:w-full xl:text-lg/8 2xl:text-2xl text-zinc-800 mb-6">
+            <p className="w-10/12 font-light text-md lg:w-full xl:text-lg/8 2xl:text-2xl text-zinc-800 mb-6">
               A forma de cuidar de quem você ama
               está aqui. Com o Seguro de Vida <span className="font-bold">APUS</span>
               digital, você garante tranquilidade e
               segurança para o futuro da sua família.
             </p>
-            <p className="w-full font-light text-md lg:w-full xl:text-lg/8 2xl:text-2xl text-zinc-800 mb-6">
+            <p className="w-10/12 font-light text-md lg:w-full xl:text-lg/8 2xl:text-2xl text-zinc-800 mb-6">
               Ao longo do contrato, todo o patrimônio
               acumulado é destinado aos beneficiários
               indicados, assegurando que os frutos do
